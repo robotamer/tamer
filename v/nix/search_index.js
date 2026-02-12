59f5feea5072090cdf8593c3dffaf04ffd095609
TaMeR <gruessle@gmail.com>
2026-02-12 21:59:29 +0300

    added v docs

---
+++ v/nix/search_index.js
@@ -0,0 +1,28 @@
+var searchModuleIndex = [
+"nix",
+];
+
+var searchIndex = [
+"error_msg",
+"hostname",
+"User",
+"get_users",
+"user_home",
+"user_id",
+"is_root",
+];
+
+var searchModuleData = [
+["","nix.html"],
+];
+
+var searchData = [
+["nix","error code lookup for shell exit error","nix.html#error_msg","fn "],
+["nix","","nix.html#hostname","fn "],
+["nix","","nix.html#User","struct "],
+["nix","","nix.html#get_users","fn "],
+["nix","","nix.html#user_home","fn "],
+["nix","","nix.html#user_id","fn "],
+["nix","is_root return true if current user is root","nix.html#is_root","fn "],
+];
+
